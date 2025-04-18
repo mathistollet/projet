525d6bf91d0e14352968ffb08982a030bff5833e
lakdhv!lki <mathis.tollet@gmail.com>
2025-04-18 16:48:45 +0200

    Update fonctioncombat.h

--- fonctioncombat.h
+++ fonctioncombat.h
@@ -1,1 +1,11 @@
+void afficherVie(Champion p1, Champion p2) {
+    printf("\n--- ÉTAT DES COMBATTANTS ---\n");
+    printf("%s : %d PV\t\t%s : %d PV\n\n", p1.nom, p1.pvc, p2.nom, p2.pvc);
+}
 
+
+void utiliserAttaque(Champion *attaquant, Champion *cible, Attaque a) {
+    printf("%s utilise %s et inflige %d dégâts à %s !\n", attaquant->nom, a.nom, a.degats, cible->nom);
+    cible->pvc= a.degats;
+    if (cible->pvc < 0) cible->pvc= 0;
+}
